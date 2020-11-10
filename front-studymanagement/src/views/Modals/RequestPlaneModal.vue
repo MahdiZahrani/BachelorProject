112b2b82aaa123cd3853f89a0a8cea88e7d7115c
mahdizahrani <mahdizahrani76@gmail.com>
2020-11-11 00:03:29 +0330

    requestPlanModal

--- front-studymanagement/src/views/Modals/RequestPlaneModal.vue
+++ front-studymanagement/src/views/Modals/RequestPlaneModal.vue
@@ -61,20 +61,90 @@
                 <v-spacer></v-spacer>
                 <v-dialog
                     v-model="CreateTimeDialog"
-                    max-width="500px"
                 >
                   <template v-slot:activator="{ on, attrs }">
                     <v-btn
                         color="primary"
                         dark
-                        class="mb-2"
+                        class="text-h6 mb-2"
                         v-bind="attrs"
                         v-on="on"
                     >
                       زمان جدید
                     </v-btn>
                   </template>
+
+                  <v-card>
+                    <v-card-title>
+                      <span class="headline">ثبت زمان جدید</span>
+                    </v-card-title>
+
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col
+                              cols="12"
+                              sm="6"
+                              md="12"
+                          >
+                            <v-select
+                                v-model="day"
+                                :items="days"
+                                menu-props="auto"
+                                label="روز"
+                                dense
+                                hide-details
+                                prepend-icon="mdi-map"
+                                single-line
+                            ></v-select>
+                          </v-col>
+                        </v-row>
+                      <v-row
+                          justify="space-around"
+                          align="center"
+                      >
+                        <v-col style="width: 350px; flex: 0 1 auto;">
+                          <h2 class="mb-3">از ساعت:</h2>
+                          <v-time-picker
+                              v-model="start"
+                              :max="end"
+                              format="24hr"
+                          ></v-time-picker>
+                        </v-col>
+                        <v-col style="width: 350px; flex: 0 1 auto;">
+                          <h2 class="mb-3">تا ساعت:</h2>
+                          <v-time-picker
+                              v-model="end"
+                              :min="start"
+                              format="24hr"
+                          ></v-time-picker>
+                        </v-col>
+                      </v-row>
+                      </v-container>
+                    </v-card-text>
+
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                          class="text-h6"
+                          color="red"
+                      >
+                        انصراف
+                      </v-btn>
+                      <v-btn
+                          class="text-h6"
+                          color="success"
+                      >
+                        ذخیره
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+
                 </v-dialog>
+
+
+
+
               </v-toolbar>
             </template>
             <template v-slot:item.actions="{ item }">
@@ -111,6 +181,9 @@ export default {
       days : ['شنبه','یکشنبه','دوشنبه','سه شنبه','چهارشنبه','پنج شنبه','جمعه'],
       dialog: true,
       CreateTimeDialog : false,
+      day:null,
+      start: null,
+      end: null,
       dataTableHeaders : [
         {text : 'روز' , value : 'day'},
         {text : 'از ساعت' , value : 'fromTime'},
